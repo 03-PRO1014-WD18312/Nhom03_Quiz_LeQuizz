cbc1cbeb8dce446b656664865c4084a7053436d8
JustHieu15 <135332703+HieuFPTAptech@users.noreply.github.com>
2023-11-25 12:47:51 +0700

    commit

--- src/admin/models/questions.php
+++ src/admin/models/questions.php
@@ -1,16 +1,27 @@
 <?php
+
+    // ADD QUESTIONS
     function addQuestions($titleQuestion, $choice1, $choice2, $choice3, $choice4, $correctAnswer, $idExam) {
         $sql = "INSERT INTO questions(title_question, choice1_question, choice2_question, choice3_question, choice4_question, correct_answer, id_exam) 
         VALUES ('$titleQuestion' , '$choice1', '$choice2', '$choice3', '$choice4', '$correctAnswer', '$idExam')";
         pdo_execute($sql);
     }
 
+    // LOAD 1 QUESTION
     function loadOneQuesion($idQuestion) {
         $sql = "SELECT * FROM questions WHERE id_question =".$idQuestion;
         $question = pdo_query_one($sql);
         return $question;
     }
 
+    // LIST QUESTIONS IN EXAM
+    function listQuestions($examId) {
+        $sql = "SELECT * FROM questions WHERE id_exam ='".$examId."' ORDER BY id_question DESC";
+        $listQuestion = pdo_query($sql);
+        return $listQuestion;
+    }
+
+    // UPDATE QUESTIONS
     function updateQuestions($titleQuestion, $choice1, $choice2, $choice3, $choice4, $correctAnswer, $idQuestion) {
         $sql = "UPDATE questions SET title_question = '".$titleQuestion."', choice1_question = '".$choice1."', 
         choice2_question = '".$choice2."', choice3_question = '".$choice3."', 
@@ -19,6 +30,7 @@
         pdo_query($sql);
     }
 
+    // DELETE QUESTIONS
     function deleteQuestions($idQuestion) {
         $sql = "DELETE FROM questions WHERE id_question =".$idQuestion;
         pdo_execute($sql);
