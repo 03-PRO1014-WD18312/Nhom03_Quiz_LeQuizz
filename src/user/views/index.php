680369fd8a10a9fadb6825e5c3a5138c240fa984
dungnm <mdung1103@gmail.com>
2023-11-25 12:39:12 +0700

    add pages user

--- src/user/views/index.php
+++ src/user/views/index.php
@@ -1,9 +1,40 @@
 <?php
+
+include '../../config/db.php';
+include '../../admin/models/subjects.php';
+
 include 'header.php';
-include 'sidebar.php';
-include 'main.php';
 
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
 
+    switch ($act) {
+        case 'login':
+            include 'login.php';
+            break;
 
+        case 'register':
+            include 'register.php';
+            break;
+
+        case 'forgotPassword':
+            include 'forgotPassword.php';
+            break;
+
+        case 'registSubject':
+            $listSubject = listSubject();
+
+            include './pages/subjects/registSubject.php';
+            break;
+
+        default:
+            include 'sidebar.php';
+            // include 'main.php';
+            break;
+    }
+} else {
+    include 'sidebar.php';
+    // include 'main.php';
+}
 
 include 'footer.php';
--- src/user/views/sidebar.php
+++ src/user/views/sidebar.php
@@ -35,7 +35,7 @@
             </a>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <a class="collapse-item" href="#">Regist Subjects</a>
+                    <a class="collapse-item" href="index.php?act=registSubject">Regist Subjects</a>
                     <a class="collapse-item" href="#">Manage Subjects</a>
                 </div>
             </div>
