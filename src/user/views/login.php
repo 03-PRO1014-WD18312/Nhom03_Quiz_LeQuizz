8f1699ca3d4a3864eeafe4d6da6c76ef8ecd9bc0
dungnm <mdung1103@gmail.com>
2023-11-26 13:33:56 +0700

    add register and login

--- src/user/views/login.php
+++ src/user/views/login.php
@@ -14,22 +14,26 @@
                                         Welcome Back!
                                     </h1>
                                 </div>
-                                <form class="user">
+                                <form action="index.php?act=login" method="POST" class="user">
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+                                        <input name="email" type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
+                                        <input name="password" type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
                                     </div>
+
+                                    <?php if (isset($notice)) { ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?php echo $notice; ?>
+                                        </div>
+                                    <?php } ?>
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
                                             <input type="checkbox" class="custom-control-input" id="customCheck" />
                                             <label class="custom-control-label" for="customCheck">Remember Me</label>
                                         </div>
                                     </div>
-                                    <a href="index.php" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
+                                    <input type="submit" class="btn btn-primary btn-user btn-block" value="Login">
                                     <hr />
                                     <a href="index.php" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i>
