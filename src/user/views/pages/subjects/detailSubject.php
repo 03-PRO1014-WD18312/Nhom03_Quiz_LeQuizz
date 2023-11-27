5bf9d5945a7ca5c037da7a73af7d31a58dd8362c
dungnm <mdung1103@gmail.com>
2023-11-27 20:59:24 +0700

    add detail subject

---
+++ src/user/views/pages/subjects/detailSubject.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">Chi tiết môn học</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <?php
+        echo "<h3>Môn: " . $loadOneSubject['name_subject'] . "</h3>";
+        ?>
+
+        <!-- listExamsBySubject -->
+        <?php
+        foreach ($listExamsBySubject as $key => $value) {
+            echo "<div class='col-12'>";
+            echo "<h4>Đề: " . $value['exam_title'] . "</h4>";
+            echo "<p>Thời gian làm bài: " . $value['exam_time_limit'] . " phút</p>";
+            echo "<p>Số lượng câu hỏi: " . $value['exam_limit_quest'] . "</p>";
+            echo "<p>Mô tả: " . $value['exam_description'] . "</p>";
+            echo "<a href='index.php?act=doExam&id=" . $value['id_exam'] . "' class='btn btn-primary'>Làm bài</a>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+
+    <div class="row"></div>
+    <div class="col-12">
+        <a href="index.php?act=registSubject" class="btn btn-primary">Quay lại</a>
+        <a href="index.php?act=registerSubject&id=<?php echo $loadOneSubject['id_subject']; ?>" class="btn btn-primary">Đăng ký môn học</a>
+    </div>
+</div>
+</div>
--- src/user/views/sidebar.php
+++ src/user/views/sidebar.php
@@ -29,8 +29,7 @@
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseTwo"
-                aria-expanded="true" aria-controls="collapseTwo">
+            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                 <i class="fas fa-fw fa-cog"></i>
                 <span>Subjects</span>
             </a>
@@ -44,13 +43,11 @@
 
         <!-- Nav Item - Utilities Collapse Menu -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseUtilities"
-                aria-expanded="true" aria-controls="collapseUtilities">
+            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                 <i class="fas fa-fw fa-wrench"></i>
                 <span>Exams</span>
             </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                data-parent="#accordionSidebar">
+            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
                     <a class="collapse-item" href="index.php?act=registExams">Regist Exams</a>
                     <a class="collapse-item" href="index.php?act=listExams">Manage Exams</a>
@@ -59,8 +56,7 @@
         </li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseFour"
-                aria-expanded="true" aria-controls="collapseFour">
+            <a class="nav-link collapsed" href="index.php" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
                 <i class="fas fa-fw fa-cog"></i>
                 <span>Blogs</span>
             </a>
@@ -74,8 +70,7 @@
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                aria-expanded="true" aria-controls="collapsePages">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
                 <i class="fas fa-fw fa-folder"></i>
                 <span>Blogs</span>
             </a>
