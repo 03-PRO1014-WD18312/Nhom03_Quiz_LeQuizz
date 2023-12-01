4759b0f127d1d264185f54a17443d5ffeece1943
JustHieu15 <135332703+HieuFPTAptech@users.noreply.github.com>
2023-12-02 04:34:48 +0700

    new src and admin pages

--- src/admin/models/examinees.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function loginAccount($email, $password)
-{
-    $sql = "SELECT * FROM examinees WHERE email_examinee ='" . $email . "' AND pass_examinee ='" . $password . "'";
-    $user = pdo_query_one($sql);
-    return $user;
-}
